142fdf3482e955eb4e85f4fe6413345063ff4238
muoten <muoten@github.com>
2024-05-16 20:23:23 +0200

    fix: url_quote ImportError

--- run.sh
+++ run.sh
@@ -7,7 +7,7 @@ echo "- set the variables ROOT_DIR and AUDIO_DIR as absolute paths"
 echo "- created the soft symlink app/static/audio pointing to your audio"
 
 echo 'Setting up Python environment...'
-python3.7 -m venv venv
+python3 -m venv venv
 source venv/bin/activate
 pip install --upgrade pip setuptools wheel
 pip install -r requirements.txt
@@ -16,11 +16,11 @@ pip install essentia-tensorflow tinytag
 echo 'Downloading models...'
 mkdir -p essentia-tf-models
 cd essentia-tf-models
-wget https://essentia.upf.edu/models/autotagging/msd/msd-musicnn-1.pb -O msd-musicnn.pb
-wget https://essentia.upf.edu/models/autotagging/msd/msd-vgg-1.pb -O msd-vgg.pb
-wget https://essentia.upf.edu/models/autotagging/mtt/mtt-musicnn-1.pb -O mtt-musicnn.pb
-wget https://essentia.upf.edu/models/autotagging/mtt/mtt-vgg-1.pb -O mtt-vgg.pb
-wget https://essentia.upf.edu/models/feature-extractors/vggish/audioset-vggish-3.pb -O audioset-vggish.pb
+wget https://essentia.upf.edu/models/autotagging/msd/msd-musicnn-1.pb -O msd-musicnn.pb --no-check-certificate
+wget https://essentia.upf.edu/models/autotagging/msd/msd-vgg-1.pb -O msd-vgg.pb --no-check-certificate
+wget https://essentia.upf.edu/models/autotagging/mtt/mtt-musicnn-1.pb -O mtt-musicnn.pb --no-check-certificate
+wget https://essentia.upf.edu/models/autotagging/mtt/mtt-vgg-1.pb -O mtt-vgg.pb --no-check-certificate
+wget https://essentia.upf.edu/models/feature-extractors/vggish/audioset-vggish-3.pb -O audioset-vggish.pb --no-check-certificate
 cd ..
 
 echo 'Processing the music collection'
